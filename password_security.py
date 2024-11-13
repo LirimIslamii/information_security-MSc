3d812852ed91449ef78247cf230f0db9153ff43a
Enis Halilaj <enis.halilaj@vodafone.com>
2024-11-13 19:40:06 +0100

    password verification for hashed passwords

--- password_security.py
+++ password_security.py
@@ -13,6 +13,16 @@ def hash_password(password):
     hash_var.set(hash_b64)
     messagebox.showinfo("Sukses", "Fjalëkalimi u hash-ua me sukses!")
 
+def verify_password():    
+    password = entry_verify.get()
+    salt = base64.b64decode(salt_var.get())
+    original_hash = base64.b64decode(hash_var.get())
+    new_hash = hashlib.pbkdf2_hmac('sha1', password.encode(), salt, 100000)
+    if new_hash == original_hash:
+        messagebox.showinfo("Sukses", "Fjalëkalimi është korrekt!")
+    else:
+        messagebox.showerror("Gabim", "Fjalëkalimi nuk përputhet!")
+
 root = tk.Tk()
 root.title("Mbrojtja e Fjalëkalimeve me PBKDF2-SHA1")
 root.geometry("500x190")
@@ -34,5 +44,12 @@ entry_salt.grid(row=1, column=1, padx=10, pady=10)
 tk.Label(root, text="Hash:").grid(row=2, column=0, sticky='w', padx=10, pady=10)
 entry_hash = tk.Entry(root, textvariable=hash_var, state='readonly', width=30)
 entry_hash.grid(row=2, column=1, padx=10, pady=10)
+
+tk.Label(root, text="Verifiko Fjalëkalimin:").grid(row=3, column=0, padx=10, pady=10)
+entry_verify = tk.Entry(root, show="*", width=35)
+entry_verify.grid(row=3, column=1, padx=10)
+
+btn_verify = tk.Button(root, text="Verifiko", command=verify_password)
+btn_verify.grid(row=3, column=2, padx=(10, 2), pady=10, sticky='w')
 
 root.mainloop()
